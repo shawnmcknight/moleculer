70f58fbc99709deab0176dc1fd168f677c0ac266
Shawn McKnight <smcknight@storis.com>
2021-05-02 14:49:43 -0400

    Expand health types

--- src/health.d.ts
+++ src/health.d.ts
@@ -1,44 +1,68 @@
+import os from "os";
+
+export interface NodeClientInfo {
+	type: "nodejs";
+	version: string;
+	langVersion: NodeJS.Process["version"];
+}
+
+export interface NodeCpuInfo {
+	load1: number;
+	load5: number;
+	load15: number;
+	cores: number;
+	utilization: number;
+}
+
+export interface NodeMemoryInfo {
+	free: number;
+	total: number;
+	percent: number;
+}
+
+export interface NodeOsInfo {
+	uptime: number;
+	type: string;
+	release: string;
+	hostname: string;
+	arch: string;
+	platform: NodeJS.Platform;
+	user: os.UserInfo<string>;
+}
+
+export interface NodeProcessInfo {
+	pid: NodeJS.Process["pid"];
+	memory: NodeJS.MemoryUsage;
+	uptime: number;
+	argv: string[];
+}
+
+export interface NodeNetworkInfo {
+	ip: string[];
+}
+
+export interface NodeDateTimeInfo {
+	now: number;
+	iso: string;
+	utc: string;
+}
+
 export interface NodeHealthStatus {
-	cpu: {
-		load1: number;
-		load5: number;
-		load15: number;
-		cores: number;
-		utilization: number;
-	};
-	mem: {
-		free: number;
-		total: number;
-		percent: number;
-	};
-	os: {
-		uptime: number;
-		type: string;
-		release: string;
-		hostname: string;
-		arch: string;
-		platform: string;
-		user: string;
-	};
-	process: {
-		pid: NodeJS.Process["pid"];
-		memory: NodeJS.MemoryUsage;
-		uptime: number;
-		argv: string[];
-	};
-	client: {
-		type: string;
-		version: string;
-		langVersion: NodeJS.Process["version"];
-	};
-	net: {
-		ip: string[];
-	};
-	time: {
-		now: number;
-		iso: string;
-		utc: string;
-	};
+	client: NodeClientInfo;
+	cpu: NodeCpuInfo;
+	mem: NodeMemoryInfo;
+	os: NodeOsInfo;
+	process: NodeProcessInfo;
+	net: NodeNetworkInfo;
+	time: NodeDateTimeInfo;
 }
 
 export function getHealthStatus(): NodeHealthStatus;
+export function getCpuInfo(): NodeCpuInfo;
+export function getMemoryInfo(): NodeMemoryInfo;
+export function getOsInfo(): NodeOsInfo;
+export function getProcessInfo(): NodeProcessInfo;
+export function getClientInfo(): NodeClientInfo;
+export function getNetworkInterfacesInfo(): NodeNetworkInfo;
+export function getDateTimeInfo(): NodeDateTimeInfo;
+
